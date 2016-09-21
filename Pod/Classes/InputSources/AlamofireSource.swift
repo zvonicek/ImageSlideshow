e763473f6931d7dfcbb098b3cc58302866c82008
Kiattisak Anoochitarom <macbaszii@gmail.com>
2016-09-21 23:31:25 +0700

    try get

--- Pod/Classes/InputSources/AlamofireSource.swift
+++ Pod/Classes/InputSources/AlamofireSource.swift
@@ -28,7 +28,7 @@ public class AlamofireSource: NSObject, InputSource {
     }
     
     public func setToImageView(_ imageView: UIImageView) {
-        Alamofire.request(self.url)
+        Alamofire.request(self.url, method: .get)
             .responseImage { response in
                 if let image = response.result.value {
                     imageView.image = image
