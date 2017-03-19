c2233fd734f2440a5b61000000248d20b1cf57e1
Petr Zvoníček <zvonicek@gmail.com>
2017-03-19 18:35:39 +0100

    Improve InputSources' placeholder handling (fixes #125)

--- ImageSlideshow/Classes/InputSources/AlamofireSource.swift
+++ ImageSlideshow/Classes/InputSources/AlamofireSource.swift
@@ -11,20 +11,28 @@ import AlamofireImage
 
 /// Input Source to image using Alamofire
 public class AlamofireSource: NSObject, InputSource {
-    var url: URL
+    /// url to load
+    public var url: URL
+    
+    /// placeholder used before image is loaded
+    public var placeholder: UIImage?
 
     /// Initializes a new source with a URL
-    /// - parameter url: a url to be loaded
-    public init(url: URL) {
+    /// - parameter url: a url to load
+    /// - parameter placeholder: a placeholder used before image is loaded
+    public init(url: URL, placeholder: UIImage? = nil) {
         self.url = url
+        self.placeholder = placeholder
         super.init()
     }
 
     /// Initializes a new source with a URL string
-    /// - parameter urlString: a string url to be loaded
-    public init?(urlString: String) {
+    /// - parameter urlString: a string url to load
+    /// - parameter placeholder: a placeholder used before image is loaded
+    public init?(urlString: String, placeholder: UIImage? = nil) {
         if let validUrl = URL(string: urlString) {
             self.url = validUrl
+            self.placeholder = placeholder
             super.init()
         } else {
             return nil
@@ -32,7 +40,7 @@ public class AlamofireSource: NSObject, InputSource {
     }
 
     public func load(to imageView: UIImageView, with callback: @escaping (UIImage) -> Void) {
-        imageView.af_setImage(withURL: self.url, placeholderImage: nil, filter: nil, progress: nil) { (response) in
+        imageView.af_setImage(withURL: self.url, placeholderImage: placeholder, filter: nil, progress: nil) { (response) in
             imageView.image = response.result.value
             if let value = response.result.value {
                 callback(value)
--- ImageSlideshow/Classes/InputSources/KingfisherSource.swift
+++ ImageSlideshow/Classes/InputSources/KingfisherSource.swift
@@ -10,30 +10,28 @@ import Kingfisher
 
 /// Input Source to image using Kingfisher
 public class KingfisherSource: NSObject, InputSource {
-    var url: URL
-    var placeholder: UIImage?
+    /// url to load
+    public var url: URL
+
+    /// placeholder used before image is loaded
+    public var placeholder: UIImage?
 
     /// Initializes a new source with a URL
     /// - parameter url: a url to be loaded
-    public init(url: URL) {
-        self.url = url
-        super.init()
-    }
-
-    /// Initializes a new source with URL and placeholder
-    /// - parameter url: a url to be loaded
-    /// - parameter placeholder: a placeholder used before image is loaded
-    public init(url: URL, placeholder: UIImage) {
+    /// - parameter placeholder: a placeholder used before image is loaded    
+    public init(url: URL, placeholder: UIImage? = nil) {
         self.url = url
         self.placeholder = placeholder
         super.init()
     }
 
     /// Initializes a new source with a URL string
-    /// - parameter urlString: a string url to be loaded    
-    public init?(urlString: String) {
+    /// - parameter urlString: a string url to load
+    /// - parameter placeholder: a placeholder used before image is loaded
+    public init?(urlString: String, placeholder: UIImage? = nil) {
         if let validUrl = URL(string: urlString) {
             self.url = validUrl
+            self.placeholder = placeholder
             super.init()
         } else {
             return nil
