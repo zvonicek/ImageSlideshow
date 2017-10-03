cc0ae9da81cfe00375e7b8d809d63cc1cf348bbe
Petr Zvoníček <zvonicek@gmail.com>
2017-10-04 00:11:20 +0200

    Use safeAreaInsets to adjust page control position

--- ImageSlideshow/Classes/Core/ImageSlideshow.swift
+++ ImageSlideshow/Classes/Core/ImageSlideshow.swift
@@ -216,8 +216,13 @@ open class ImageSlideshow: UIView {
             pageControl.isHidden = self.images.count < 2
         }
 
+        var pageControlBottomInset: CGFloat = 12.0
+        if #available(iOS 11.0, *) {
+            pageControlBottomInset += self.safeAreaInsets.bottom
+        }
+
         pageControl.frame = CGRect(x: 0, y: 0, width: frame.size.width, height: 10)
-        pageControl.center = CGPoint(x: frame.size.width / 2, y: frame.size.height - 12.0)
+        pageControl.center = CGPoint(x: frame.size.width / 2, y: frame.size.height - pageControlBottomInset)
     }
 
     /// updates frame of the scroll view and its inner items
