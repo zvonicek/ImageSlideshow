096f899f616a877ac6091c97ce8cd58a493f8aac
Petr Zvoníček <zvonicek@gmail.com>
2018-09-26 12:34:37 +0200

    Migrate example project to Swift 4.2

--- Example/ImageSlideshow/ViewController.swift
+++ Example/ImageSlideshow/ViewController.swift
@@ -28,7 +28,7 @@ class ViewController: UIViewController {
 
         slideshow.slideshowInterval = 5.0
         slideshow.pageIndicatorPosition = .init(horizontal: .center, vertical: .under)
-        slideshow.contentScaleMode = UIViewContentMode.scaleAspectFill
+        slideshow.contentScaleMode = UIView.ContentMode.scaleAspectFill
 
         let pageControl = UIPageControl()
         pageControl.currentPageIndicatorTintColor = UIColor.lightGray
