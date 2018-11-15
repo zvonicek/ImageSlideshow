8f8447ea1812484b9f196febc3d09d24ba191221
Zhuo Hong Wei <hongwei@techinasia.com>
2018-11-15 08:40:45 +0800

    Added missing `weak self`

--- ImageSlideshow/Classes/InputSources/AlamofireSource.swift
+++ ImageSlideshow/Classes/InputSources/AlamofireSource.swift
@@ -41,7 +41,7 @@ public class AlamofireSource: NSObject, InputSource {
     }
 
     public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
-        imageView.af_setImage(withURL: self.url, placeholderImage: placeholder, filter: nil, progress: nil) { (response) in                                                              
+        imageView.af_setImage(withURL: self.url, placeholderImage: placeholder, filter: nil, progress: nil) { [weak self] (response) in                                                              
             if response.result.isSuccess {
                 callback(response.result.value)
             } else if let strongSelf = self {
