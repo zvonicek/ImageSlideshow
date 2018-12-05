0702de0a2a5775d485da5bddb45a4532ae4bb078
Ernesto Rivera <rivera.ernesto@gmail.com>
2018-12-05 17:08:58 -0400

    Remove never used calculatePictureFrame private function

--- ImageSlideshow/Classes/Core/ImageSlideshowItem.swift
+++ ImageSlideshow/Classes/Core/ImageSlideshowItem.swift
@@ -175,25 +175,6 @@ open class ImageSlideshowItem: UIScrollView, UIScrollViewDelegate {
         return CGSize(width: frame.width, height: frame.height)
     }
 
-    fileprivate func calculatePictureFrame() {
-        let boundsSize: CGSize = bounds.size
-        var frameToCenter: CGRect = imageView.frame
-
-        if frameToCenter.size.width < boundsSize.width {
-            frameToCenter.origin.x = (boundsSize.width - frameToCenter.size.width) / 2
-        } else {
-            frameToCenter.origin.x = 0
-        }
-
-        if frameToCenter.size.height < boundsSize.height {
-            frameToCenter.origin.y = (boundsSize.height - frameToCenter.size.height) / 2
-        } else {
-            frameToCenter.origin.y = 0
-        }
-
-        imageView.frame = frameToCenter
-    }
-
     fileprivate func calculatePictureSize() -> CGSize {
         if let image = imageView.image, imageView.contentMode == .scaleAspectFit {
             let picSize = image.size
