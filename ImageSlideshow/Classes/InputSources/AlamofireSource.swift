ac70a233ca90a5795a5a5a2dd6ec887a85a8f2fc
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2019-10-02 15:58:08 -0400

    Update AlamofireSource.swift

--- ImageSlideshow/Classes/InputSources/AlamofireSource.swift
+++ ImageSlideshow/Classes/InputSources/AlamofireSource.swift
@@ -1,74 +1,64 @@
 //
-//  KingfisherSource.swift
+//  AlamofireSource.swift
 //  ImageSlideshow
 //
-//  Created by feiin
+//  Created by Petr Zvoníček on 14.01.16.
 //
 //
 
 import Foundation
 import UIKit
 import ImageSlideshow
-import Kingfisher
+import Alamofire
+import AlamofireImage
 
-/// Input Source to image using Kingfisher
-public class KingfisherSource: NSObject, InputSource {
+/// Input Source to image using Alamofire
+@objcMembers
+public class AlamofireSource: NSObject, InputSource {
     /// url to load
     public var url: URL
-
+    
     /// placeholder used before image is loaded
     public var placeholder: UIImage?
-    
-    /// options for displaying, ie. [.transition(.fade(0.2))]
-    public var options: KingfisherOptionsInfo?
 
     /// Initializes a new source with a URL
-    /// - parameter url: a url to be loaded
+    /// - parameter url: a url to load
     /// - parameter placeholder: a placeholder used before image is loaded
-    /// - parameter options: options for displaying
-    public init(url: URL, placeholder: UIImage? = nil, options: KingfisherOptionsInfo? = nil) {
+    public init(url: URL, placeholder: UIImage? = nil) {
         self.url = url
         self.placeholder = placeholder
-        self.options = options
         super.init()
     }
 
     /// Initializes a new source with a URL string
     /// - parameter urlString: a string url to load
     /// - parameter placeholder: a placeholder used before image is loaded
-    /// - parameter options: options for displaying
-    public init?(urlString: String, placeholder: UIImage? = nil, options: KingfisherOptionsInfo? = nil) {
+    public init?(urlString: String, placeholder: UIImage? = nil) {
         if let validUrl = URL(string: urlString) {
             self.url = validUrl
             self.placeholder = placeholder
-            self.options = options
             super.init()
         } else {
             return nil
         }
     }
-    
-    /// Load an image to an UIImageView
-    ///
-    /// - Parameters:
-    ///   - imageView: UIImageView that receives the loaded image
-    ///   - callback: Completion callback with an optional image
-    @objc
+
     public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
-        imageView.kf.setImage(with: self.url, placeholder: self.placeholder, options: self.options, progressBlock: nil) { result in
-            switch result {
+        imageView.af_setImage(withURL: self.url, placeholderImage: placeholder, filter: nil, progress: nil) { [weak self] (response) in
+            switch response.result {
             case .success(let image):
-                callback(image.image)
-            case .failure:
-                callback(nil)
+                callback(image)
+            case .failure(_):
+                if let strongSelf = self {
+                    callback(strongSelf.placeholder)
+                } else {
+                    callback(nil)
+                }
             }
         }
     }
-    
-    /// Cancel an image download task
-    ///
-    /// - Parameter imageView: UIImage view with the download task that should be canceled
+
     public func cancelLoad(on imageView: UIImageView) {
-        imageView.kf.cancelDownloadTask()
+        imageView.af_cancelImageRequest()
     }
 }
